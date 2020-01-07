f8827553d06d8c1438512e5b5ae63affe4095e93
PRAKASHTELHARKAR <PRAKASHTELHARKAR@DESKTOP-5IAPKIA.in.ibm.com>
2020-01-07 19:57:26 +0530

    Changes are made into the Google.java class.

--- Furlough_Project/src/test/java/com/ibm/automation/testing/Google.java
+++ Furlough_Project/src/test/java/com/ibm/automation/testing/Google.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
+//Google Search Demo Class
 public class Google {
 	
 	WebDriver driver;
